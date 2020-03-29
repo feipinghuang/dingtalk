09d452f110d158ed55ddfd38c2e61d2274a914ac
feipinghuang <feipinghuang@gmail.com>
2020-03-29 19:31:03 +0800

    scale

--- lib/dingtalk/api/process_instance.rb
+++ lib/dingtalk/api/process_instance.rb
@@ -1,10 +1,10 @@
 module Dingtalk
   module Api
     module ProcessInstance
-      def process_instance_create(user_id:, dept_id:, form_component_values:)
+      def process_instance_create(user_id:, dept_id:, process_code: Dingtalk.config.process_code,  form_component_values:)
         body = {
           agent_id: Dingtalk.config.agentid,
-          process_code: Dingtalk.config.process_code,
+          process_code: process_code,
           originator_user_id: user_id,
           dept_id: dept_id,
           form_component_values: form_component_values
